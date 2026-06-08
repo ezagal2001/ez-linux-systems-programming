1bf418078c1784f4ad7d3e83cb45de9970b90964
Emmanuel Zagal <ezagal2001@gmail.com>
2026-06-08 16:14:14 -0700

    Added fileio functions

--- ez-fileio1/include/ez_fio.h
+++ ez-fileio1/include/ez_fio.h
@@ -5,6 +5,9 @@
 #include "common.h"
 
 int ezfio1(const char *pname, const char *m);
+int ez_set_seek(FILE *stream, long offest, int whence);
+long ez_tell_seek(FILE *stream);
+void ez_rewind_seek(FILE *stream);
 
 
 #endif
--- ez-fileio1/src/main.c
+++ ez-fileio1/src/main.c
@@ -4,6 +4,8 @@ int main(void)
 {
     /* File to open and the mode */
     char *path = "ezfile.txt";
+
+    /* append mode, I think. */
     char *mode = "a";
     
     int r = ezfio1(path, mode);
